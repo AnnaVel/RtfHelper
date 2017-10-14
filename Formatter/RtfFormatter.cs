78da76e8022afd500bdcbc8c72bc76dd07462463
Anna Velcheva <annasg@gmail.com>
2017-10-14 23:54:32 +0300

    Added command name.

--- Formatter/RtfFormatter.cs
+++ Formatter/RtfFormatter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -11,29 +10,19 @@ namespace RtfHelper.Formatter
         {
             StringBuilder newText = new StringBuilder(allText.Length);
 
-            int indentLevel = 0;
-            string indent = "    ";
+            newText.Append(allText[0]);
 
-            for (int i = 0; i < allText.Length; i++)
+            for (int i = 1; i < allText.Length; i++)
             {
                 if (allText[i] == '{')
                 {
                     newText.AppendLine();
-
-                    for (int u = 0; u < indentLevel; u++)
-                    {
-                        newText.Append(indent);
-                    }
-
-                    indentLevel++;
                 }
 
-                if (allText[i] == '}')
+                if (Environment.NewLine.All(c => allText[i] != c))
                 {
-                    indentLevel--;
+                    newText.Append(allText[i]);
                 }
-
-                newText.Append(allText[i]);
             }
 
             return newText.ToString();
