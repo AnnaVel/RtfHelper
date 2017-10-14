ab88b487f89b295b2c37906e291ee47eafa4858a
Anna Velcheva <annasg@gmail.com>
2017-10-14 21:59:56 +0300

    Initial commit

---
+++ Formatter/RtfFormatter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RtfHelper.Formatter
+{
+    internal class RtfFormatter
+    {
+        public string GetFormattedText(string allText)
+        {
+            StringBuilder newText = new StringBuilder(allText.Length);
+
+            int indentLevel = 0;
+            string indent = "    ";
+
+            for (int i = 0; i < allText.Length; i++)
+            {
+                if (allText[i] == '{')
+                {
+                    newText.AppendLine();
+
+                    for (int u = 0; u < indentLevel; u++)
+                    {
+                        newText.Append(indent);
+                    }
+
+                    indentLevel++;
+                }
+
+                if (allText[i] == '}')
+                {
+                    indentLevel--;
+                }
+
+                newText.Append(allText[i]);
+            }
+
+            return newText.ToString();
+        }
+    }
+}
---
+++ Guard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Kbg.NppPluginNET
+{
+    public static class Guard
+    {
+        public static void ThrowExceptionIfNull(object parameter, string parameterName)
+        {
+            if(parameter == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+        }
+    }
+}
