c5ef689c412904bcb47151e44b15a616175626e1
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2019-05-01 10:58:48 -0300

    update PluginInfrastructure to v0.94.00

--- RtfHelper/PluginInfrastructure/UnmanagedExports.cs
+++ RtfHelper/PluginInfrastructure/UnmanagedExports.cs
@@ -1,4 +1,4 @@
-﻿// NPP plugin platform for .Net v0.93.96 by Kasper B. Graversen etc.
+﻿// NPP plugin platform for .Net v0.94.00 by Kasper B. Graversen etc.
 using System;
 using System.Runtime.InteropServices;
 using Kbg.NppPluginNET.PluginInfrastructure;
@@ -59,7 +59,7 @@ namespace Kbg.NppPluginNET
             }
             else
             {
-	            Main.OnNotification(notification);
+                Main.OnNotification(notification);
             }
         }
     }
